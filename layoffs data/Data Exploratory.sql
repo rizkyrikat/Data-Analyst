9707176de60e0d9e989de8e6072f32f2b5854d72
Kimontetsu <rizkyrikat@gmail.com>
2025-09-23 22:01:57 +0700

    Add files via upload

---
+++ layoffs data/Data Exploratory.sql
@@ -0,0 +1,85 @@
+-- Exploratory Data Analyst
+
+select *
+from layoffs_staging2;
+
+select max(total_laid_off), max(percentage_laid_off)
+from layoffs_staging2;
+
+select *
+from layoffs_staging2
+where percentage_laid_off = 1
+order by funds_raised_millions desc;
+
+select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+select min(`date`), max(`date`)
+from layoffs_staging2;
+
+select country, sum(total_laid_off)
+from layoffs_staging2
+group by country
+order by 2 desc;
+
+select *
+from layoffs_staging2;
+
+select year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by year(`date`)
+order by 1 desc;
+
+select stage, sum(total_laid_off)
+from layoffs_staging2
+group by stage
+order by 2 desc;
+
+select company, sum(percentage_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+select substring(`date`,1,7) as `month`, sum(total_laid_off)
+from layoffs_staging2
+where substring(`date`,1,7) is not null
+group by `month`
+order by 1 asc;
+
+with rolling_total as
+(
+select substring(`date`,1,7) as `month`, sum(total_laid_off) as total_off
+from layoffs_staging2
+where substring(`date`,1,7) is not null
+group by `month`
+order by 1 asc
+)
+select `month`, total_off
+,sum(total_off) over(order by `month`) as rolling_total
+from rolling_total;
+
+select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+select company, year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by company, year(`date`)
+order by 3 desc;
+
+with Company_Years (company, year, total_laid_off) as
+(
+select company, year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by company, year(`date`)
+), Company_Year_Rank as
+(select *, dense_rank() over(partition by year order by total_laid_off desc) as Ranking
+from Company_Years
+where year is not null
+)
+select * 
+from Company_Year_Rank
+where Ranking <= 5;
